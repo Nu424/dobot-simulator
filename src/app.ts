e1297ad04be16b3c26adc77aa5cce1992b54b892
Nuva424 <murataka0424@gmail.com>
2025-03-22 18:11:45 +0900

    feat: DMSWrapperの動作を確認するコードを作成した

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,62 @@
-import { DobotMagicianSimulator } from "./DobotMagicianSimulator";
+// ----------
+// ---DMSWrapperのコード
+// ----------
+import { DMSWrapper } from "./DMSWrapper";
 
 const rootElement = document.getElementById("root") as HTMLDivElement;
-const simulator = new DobotMagicianSimulator(rootElement);
-simulator.init();
+const simulator = new DMSWrapper(rootElement);
+(async () => {
+    await simulator.initiate();
+    await simulator.go_home();
+})();
 
 document.addEventListener("contextmenu", async (e) => {
     e.preventDefault();
-    simulator.updateArmByXYZ({ x: 200, y: 0, z: 0 });
-    await simulator.updateArmByXYZAnimation({ x: 200, y: 100, z: 0 }, 100);
-
-    // simulator.updateArmByXYZ({ x: 200, y: 0, z: 0 });
-    // if (simulator.arm2Mesh && simulator.arm1Mesh) {
-    //     simulator.arm1Mesh.rotation.x = 30 * Math.PI / 180;
-    //     simulator.arm2Mesh.rotation.x = 30 * Math.PI / 180;
-    // }
+    const randomPos = {
+        x: 200 + Math.random() * 100,
+        y: Math.random() * 200 - 100,
+        z: Math.random() * 100,
+    }
+    console.log(randomPos);
+    // await simulator.move_arm(randomPos, 100);
+    await simulator.move_arm_safe(randomPos, 100, 100);
+    console.log("finished")
 });
+document.addEventListener("dblclick", async (e) => {
+    e.preventDefault();
+    await simulator.go_home();
+});
+
+// ----------
+// ---DobotMagicianSimulatorのコード
+// ----------
+// import { DobotMagicianSimulator } from "./DobotMagicianSimulator";
+
+// const rootElement = document.getElementById("root") as HTMLDivElement;
+// // const simulator = new DobotMagicianSimulator(rootElement);
+// // simulator.init();
+
+// document.addEventListener("contextmenu", async (e) => {
+//     e.preventDefault();
+//     // ---逆運動学のテスト
+//     // const ikTargetPos = { x: 200, y: 0, z: 0 }
+//     // const ikResult = simulator.calcInverseKinematics(ikTargetPos)
+//     // console.log(ikResult)
+//     // simulator.updateArmByXYZ(ikTargetPos);
+
+//     // ---順運動学のテスト
+//     // const fkTargetAngles = { j1: 0, j2: 25.403778, j3: 56.054351 }
+//     // const fkResult = simulator.calcForwardKinematics(fkTargetAngles)
+//     // console.log(fkResult)
+//     // simulator.updateArmByDobotAngles(fkTargetAngles);
+
+//     // ---アニメーションのテスト
+//     await simulator.animateArmByXYZ({ x: 200, y: 100, z: 0 }, 100);
+//     console.log("finished")
+
+//     // simulator.updateArmByXYZ({ x: 200, y: 0, z: 0 });
+//     // if (simulator.arm2Mesh && simulator.arm1Mesh) {
+//     //     simulator.arm1Mesh.rotation.x = 30 * Math.PI / 180;
+//     //     simulator.arm2Mesh.rotation.x = 30 * Math.PI / 180;
+//     // }
+// });
